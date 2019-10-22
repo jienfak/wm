951bb196b719ae5d8a29fd50a4314129f07745e1
jienfak <jienfak@protonmail.com>
2019-10-22 16:00:35 +0500

    Basic functionality added.

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ So get it! */
 #include <X11/cursorfont.h> 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MODKEY (Mod4Mask)
 #define XKILL_HOTKEY_STR ("Escape")
@@ -27,15 +28,22 @@ So get it! */
 #define NATIVE_HOTKEY_STR ("Backspace")
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
+enum{ MouseWinMoveFlag, MouseWinResizeFlag };
+
 unsigned int strkey(Display *dpy, char *str){
 	return XKeysymToKeycode(dpy, XStringToKeysym(str)) ;
 }
 
-unsigned int grabkey(Display *dpy, Window rw, char *str){
+unsigned int grabkey( Display *dpy, Window rw, char *str){
 	unsigned int key = strkey(dpy,str) ;
 	XGrabKey( dpy, key, MODKEY, rw,
 		True, GrabModeAsync, GrabModeAsync );
 	return key ;
+}
+
+void grabbutton(Display *dpy, Window rw, int button){
+	XGrabButton( dpy, button, MODKEY, rw, True, ButtonPressMask|ButtonReleaseMask, GrabModeAsync,
+          	GrabModeAsync, None, None );
 }
 
 int main(int argc, char argv[]){
@@ -45,6 +53,9 @@ int main(int argc, char argv[]){
 	Window rw;
 	/* Cursor shape object. */
 	Cursor cur;
+	int but;
+	bool mouse_flags[] = { false, false } ;
+	Window sw;
 	/* Buffer to get window attributes. */
 	XWindowAttributes attr;
 	/* Buffer to remember delta cursor-X and cursor-Y. */
@@ -67,12 +78,12 @@ int main(int argc, char argv[]){
 		unsigned int native_key = grabkey(dpy, rw, NATIVE_HOTKEY_STR) ;
 
 	/* Mouse bindings. */
-	XGrabButton( dpy, 1, MODKEY, rw, True, ButtonPressMask, GrabModeAsync,
-          	GrabModeAsync, None, None );
-	XGrabButton(dpy, 2, MODKEY,  rw, True, ButtonPressMask, GrabModeAsync,
-		GrabModeAsync, None, None );
-	XGrabButton( dpy, 3, MODKEY, rw, True, ButtonPressMask, GrabModeAsync,
-		GrabModeAsync, None, None );
+	grabbutton(dpy, rw, 1);
+	grabbutton(dpy, rw, 2);
+	grabbutton(dpy, rw, 3);
+	grabbutton(dpy, rw, 4);
+	grabbutton(dpy, rw, 5);
+
 	/* Cursor creating. */
 	cur = XCreateFontCursor(dpy, XC_left_ptr) ;
 	XDefineCursor(dpy, rw, cur); 
@@ -80,36 +91,108 @@ int main(int argc, char argv[]){
 		XNextEvent(dpy, &ev);
 		if( ev.type == KeyPress /* && ev.xkey.subwindow != None */ ){
 			/* Super key is pressed. */ 
+			system("xmodmap $XMODMAP &");
 			unsigned int key = ev.xkey.keycode ;
 			if( key == menu_cmd_key){
-					/* The only way to call programs. */
-					system("menu_cmd&");
+				/* The only way to call programs. */
+				system("menu_cmd &");
 			}else if( key == xkill_key ){
-					/* The only way to kill programs. */
-					system("xkill -frame&");
+				/* The only way to kill programs. */
+
 			}else if( key == dvorak_key ){
-					system("setxkbmap $DVORAK_KEYBOARD_LAYOUT &");
+				system("setxkbmap $DVORAK_KEYBOARD_LAYOUT &");
 			}else if( key == dvp_key ){
-					system("setxkbmap $DVP_KEYBOARD_LAYOUT &");
+				system("setxkbmap $DVP_KEYBOARD_LAYOUT &");
 			}else if( key == native_key ){
-					system("setxkbmap $NATIVE_KEYBOARD_LAYOUT &");
+				system("setxkbmap $NATIVE_KEYBOARD_LAYOUT &");
 			}else if( key == qwerty_key ){
-					system("setxkbmap $QWERTY_KEYBOARD_LAYOUT &");
+				system("setxkbmap $QWERTY_KEYBOARD_LAYOUT &");
 			}
-			system("xmodmap $XMODMAP");
-		}else if(ev.type == ButtonPress && ev.xbutton.subwindow != None){
-				if ( ev.xbutton.button == 2 ){
-					/* If middle button pressed. */
-					/* Raising window. */
-					XRaiseWindow(dpy, ev.xkey.subwindow);
-				}
-				/* Right or left button is pressed. */
-				XGrabPointer( dpy, ev.xbutton.subwindow, True,
-					PointerMotionMask|ButtonReleaseMask, GrabModeAsync,
+
+		}else if(ev.type == ButtonPress ){
+			but = ev.xbutton.button ;
+			sw = ev.xbutton.subwindow ;
+			XGrabPointer( dpy, sw, True,
+			PointerMotionMask|ButtonReleaseMask, GrabModeAsync,
 				GrabModeAsync, None, cur, CurrentTime );
-				XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
-				start = ev.xbutton;
-		
+			XGetWindowAttributes(dpy, sw, &attr);
+			start = ev.xbutton;
+			switch( but ){
+				case 1 :
+					if( sw != None ){
+						mouse_flags[MouseWinMoveFlag] = true ;
+						mouse_flags[MouseWinResizeFlag] = false ;
+					}else{
+					}
+				break;
+
+				case 2 :
+					if( sw != None ){
+						system("closecurwin");
+					}else{
+					}
+				break;
+
+				case 3 :
+					if( sw != None ){
+						mouse_flags[MouseWinResizeFlag] = true ;
+						mouse_flags[MouseWinMoveFlag] = false ;
+					}else{
+					}
+				break;
+
+				case 4 :
+					if( sw != None ){
+						XRaiseWindow(dpy, sw);
+					}else{
+					}
+				break;
+					
+				case 5 :
+					if( sw != None ){
+						XLowerWindow(dpy, sw);
+					}else{
+					}
+				break;
+
+			}
+		}else if(ev.type == ButtonRelease) {
+			but = ev.xbutton.button ;
+			sw = ev.xbutton.subwindow ;
+			XUngrabPointer(dpy, CurrentTime);
+			switch(but){
+					case 1 :
+						if( sw != None ){
+							mouse_flags[MouseWinMoveFlag] = false ;
+						}else{
+						}
+					break;
+
+					case 2 :
+						if( sw != None){
+						}else{
+						}
+					break;
+
+					case 3 :
+						if( sw != None ){
+							mouse_flags[MouseWinResizeFlag] = false ;
+						}else{
+						}
+					break;
+
+					case 4 :
+						if( sw != None ){
+						}else{
+						}
+					break;
+					
+					case 5 :
+						if( sw != None ){
+						}else{
+						}
+					break;
+			}
 		} else if( ev.type == MotionNotify ) {
 			/* Window resizing. */
 			int xdiff, ydiff;
@@ -118,13 +201,11 @@ int main(int argc, char argv[]){
 			xdiff = ev.xbutton.x_root - start.x_root ;
 			ydiff = ev.xbutton.y_root - start.y_root ;
 			XMoveResizeWindow(dpy, ev.xmotion.window,
-				attr.x + (start.button==1 ? xdiff : 0),
-				attr.y + (start.button==1 ? ydiff : 0 ),
-				MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
-				MAX(1, attr.height + (start.button==3 ? ydiff : 0)) );
-		} else if(ev.type == ButtonRelease) {
-			XUngrabPointer(dpy, CurrentTime);
-		}
+				attr.x + (mouse_flags[MouseWinMoveFlag] ? xdiff : 0),
+				attr.y + (mouse_flags[MouseWinMoveFlag] ? ydiff : 0 ),
+				MAX(1, attr.width + (mouse_flags[MouseWinResizeFlag] ? xdiff : 0)),
+				MAX(1, attr.height + (mouse_flags[MouseWinResizeFlag] ? ydiff : 0)) );
+		} 
 	}
 	XCloseDisplay(dpy);
 	return 0 ;
